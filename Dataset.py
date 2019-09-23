db27b6a887cbd461805707921e782fedc1643161
Srirakshith Betageri <sbetageri111@gmail.com>
2019-09-23 14:19:26 -0500

    Dataset and skeleton of training.

---
+++ Dataset.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+from PIL import Image
+
+class HistDataset(tf.keras.utils.Sequence):
+    TRAIN_SET = 9
+    VAL_SET = 18
+    TEST_SET = 27
+    def __init__(self, dataframe, img_dir, dataset_flag=TRAIN_SET, img_dim=(96, 96)):
+        super(HistDataset, self).__init__()
+        self.df = dataframe
+        self.img_dir = img_dir
+        self.dataset_flat = dataset_flag
+        self.batch_size = 4
+        self.img_dim = img_dim
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        data = self.df.iloc[idx]
+        img_id = data['id'].values[0]
+        label = data['label'].values[0]
+
+        img = Image.open(self.img_dir + img_id + '.tif')
+        img = img.resize(self.img_dim)
+        imgs = [
+            np.array(img.transpose(Image.FLIP_LEFT_RIGHT)),
+            np.array(img.transpose(Image.FLIP_TOP_BOTTOM)),
+            np.array(img.transpose(Image.ROTATE_90)),
+            np.array(img)
+        ]
+
+        ## ToDo
+        ## Do we normalise all the images?
+
+        labels = [label] * self.batch_size
+
+        t_imgs = tf.convert_to_tensor(imgs)
+        t_labels = tf.convert_to_tensor(labels)
+
+        return t_imgs, t_labels
+
+
+
---
+++ Train.py
@@ -0,0 +1,33 @@
+import pathlib
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+def get_stats(img_dir):
+    p = pathlib.Path(img_dir)
+    avg_dim = [-1, -1, -1]
+    max_dim = [-1, -1, -1]
+    min_dim = [-1, -1, -1]
+    for idx, path in enumerate(p.glob('*.tif')):
+        img = Image.open(path)
+        img = np.array(img)
+        dim = img.shape
+        idx += 1
+        avg_dim[0] = (avg_dim[0] * (idx - 1) + dim[0]) / idx
+        avg_dim[1] = (avg_dim[1] * (idx - 1) + dim[1]) / idx
+        avg_dim[2] = (avg_dim[2] * (idx - 1) + dim[2]) / idx
+
+        max_dim[0] = dim[0] if dim[0] > max_dim[0] else max_dim[0]
+        max_dim[1] = dim[1] if dim[1] > max_dim[1] else max_dim[1]
+        max_dim[2] = dim[2] if dim[2] > max_dim[2] else max_dim[2]
+
+        min_dim[0] = dim[0] if dim[0] > min_dim[0] else min_dim[0]
+        min_dim[1] = dim[1] if dim[1] > min_dim[1] else min_dim[1]
+        min_dim[2] = dim[2] if dim[2] > min_dim[2] else min_dim[2]
+
+    print(avg_dim)
+    print(max_dim)
+    print(min_dim)
+
+if __name__ == '__main__':
+   pass
