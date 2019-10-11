ca857317be897f014347337d1f6574d33a3d9a3f
Srirakshith Betageri <sbetageri111@gmail.com>
2019-10-11 00:37:56 -0500

    Use Residual Blocks.

---
+++ models/ResNet.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from models.ResNetBlock import ResNetBlock as Block
+
+class ResNet(tf.keras.Model):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.conv = tf.keras.layers.Conv2D(filters=32, kernel_size=5)
+        self.r_block1 = Block()
+        self.r_block2 = Block()
+
+        self.pool_block1 = Block(pool=True)
+        self.r_block3 = Block()
+        self.r_block4 = Block()
+
+        self.pool_block2 = Block(filters=64, pool=True)
+        self.r_block5 = Block(filters=64)
+        self.r_block6 = Block(filters=64)
+
+        self.pool_block3 = Block(filters=128, pool=True)
+        self.r_block7 = Block(filters=128)
+        self.r_block8 = Block(filters=128)
+        self.pool_block4 = Block(filters=128, pool=True)
+
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
+
+    def call(self, x):
+        x = self.conv(x)
+        x = self.r_block1(x)
+        x = self.r_block2(x)
+
+        x = self.pool_block1(x)
+        x = self.r_block3(x)
+        x = self.r_block4(x)
+
+        x = self.pool_block2(x)
+        x = self.r_block5(x)
+        x = self.r_block6(x)
+
+        x = self.pool_block3(x)
+        x = self.r_block7(x)
+        x = self.r_block8(x)
+        x = self.pool_block4(x)
+
+        x = self.flatten(x)
+        x = self.dense(x)
+
+        return x
